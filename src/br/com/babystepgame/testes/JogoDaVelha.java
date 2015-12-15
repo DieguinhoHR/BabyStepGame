26d619ea63a265df7d0bd8f9970be533b379a71b
diego <diego_henriquerodrigues@hotmail.com>
2015-12-15 20:16:15 -0200

    InserirBolaForadaMatrizI

--- src/br/com/babystepgame/testes/JogoDaVelha.java
+++ src/br/com/babystepgame/testes/JogoDaVelha.java
@@ -3,8 +3,13 @@ package br.com.babystepgame.testes;
 public class JogoDaVelha {
 	int [][] velha = {{0, 0, 0}, { 0, 0, 0}, { 0, 0, 0}};	
 	
-	public void inserirBola(int i, int j) {
+	public boolean inserirBola(int i, int j) {
+		if (i < 0 || i > 2)
+			return false;		
+		
 		velha[i][j] = 1;
+		
+		return true;
 	}
 
 	public boolean inserirXis(int i, int j) {
--- src/br/com/babystepgame/testes/JogoDaVelhaTest.java
+++ src/br/com/babystepgame/testes/JogoDaVelhaTest.java
@@ -15,24 +15,63 @@ public class JogoDaVelhaTest {
 	
 	@Test
 	public void testDeveInserirBolaNaPosicao00() {
-		jogoDaVelha.inserirBola(0, 0);
-		
+		jogoDaVelha.inserirBola(0, 0);		
 		assertEquals(1, jogoDaVelha.velha[0][0]);				
 	}
 
 	@Test
 	public void testDeveInserirXisNaPosicao00() {
-		jogoDaVelha.inserirXis(0, 0);
-		
+		jogoDaVelha.inserirXis(0, 0);		
 		assertEquals(2, jogoDaVelha.velha[0][0]);				
 	}	
 	
 	@Test
-	public void testDeveInserirBolaForaDaMatriz() {		
-		boolean retornoDoInserirXis = jogoDaVelha.inserirXis(4, 0);
-		
-		assertFalse(retornoDoInserirXis);
-		
-		//assertEquals(2, jogoDaVelha.velha[4][0]);				
+	public void testDeveInserirXisForaDaMatrizJ() {		
+		boolean retornoDoInserirXis = jogoDaVelha.inserirXis(4, 0);		
+		assertFalse(retornoDoInserirXis);					
 	}	
+	
+	@Test
+	public void testDeveInserirXisNaMatrizL() {		
+		boolean retornoDoInserirXis = jogoDaVelha.inserirXis(0, 4);		
+		assertFalse(retornoDoInserirXis);					
+	}	
+	
+	@Test
+	public void testDeveInserirXisDentroDaMatrizL() {		
+		boolean retornoDoInserirXis = jogoDaVelha.inserirXis(2, 0);		
+		assertTrue(retornoDoInserirXis);					
+	}
+
+	@Test
+	public void testDeveInserirXisDentroDaMatrizJ() {		
+		boolean retornoDoInserirXis = jogoDaVelha.inserirXis(0, 2);		
+		assertTrue(retornoDoInserirXis);					
+	}
+	
+	@Test
+	public void testDeveInserirBolaForaDaMatrizI() {
+		boolean retornoDoInserirXis = jogoDaVelha.inserirBola(4, 0);		
+		assertFalse(retornoDoInserirXis);					
+	}
+	
+	/*
+	@Test
+	public void testDeveInserirBolaNaMatrizL() {		
+		boolean retornoDoInserirXis = jogoDaVelha.inserirXis(0, 4);		
+		assertFalse(retornoDoInserirXis);					
+	}	
+	
+	@Test
+	public void testDeveInserirBolaDentroDaMatrizL() {		
+		boolean retornoDoInserirXis = jogoDaVelha.inserirXis(2, 0);		
+		assertTrue(retornoDoInserirXis);					
+	}
+
+	@Test
+	public void testDeveInserirBolaDentroDaMatrizJ() {		
+		boolean retornoDoInserirXis = jogoDaVelha.inserirXis(0, 2);		
+		assertTrue(retornoDoInserirXis);					
+	}	
+	*/
 }
